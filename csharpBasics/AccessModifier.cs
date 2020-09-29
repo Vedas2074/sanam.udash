f7bceff7825461ee2a7843d1ec3076f07b0bc6ac
sanam.udash55 <shaw.udash@gmail.com>
2020-09-29 10:26:31 +0545

    learned access modifier

---
+++ csharpBasics/AccessModifier.cs
@@ -0,0 +1,26 @@
+namespace AccessModifierAndMethods
+{
+
+internal class AccessModifier
+{
+    int x;
+    int y;
+
+    // internal access modifier
+    internal void DoSomething()
+    {
+
+    }
+    // public access modifier  
+    public int CalculateAverage(int[] numbers)
+
+    {
+        //calculate average
+
+        return 0;
+    }
+
+
+}
+
+}
--- csharpBasics/Methods.cs
+++ csharpBasics/Methods.cs
@@ -1,77 +1,81 @@
 
 using System;
 using System.Linq;
-class Methods
+namespace AccessModifierAndMethods
 {
-    void PrintInfo()
-    {
-        Console.WriteLine("printing Something.....");
-            
-            
-            sum(2.3f,3.3f);
-            sum (3.3f, 2.3f , 44.4f);
 
-            Greet("sanam","Mr");
+
+    class Methods
+    {
+        void PrintInfo()
+        {
+            Console.WriteLine("printing Something.....");
+
+
+            sum(2.3f, 3.3f);
+            sum(3.3f, 2.3f, 44.4f);
+
+            Greet("sanam", "Mr");
             Greet("sita");
 
-            byte[] numbers = {2,3,4,5,23,1,33};
-            
-            (byte , byte) result = FindMinMax(numbers);
+            byte[] numbers = { 2, 3, 4, 5, 23, 1, 33 };
 
-            PrintCustomerDetails(age: 23 , name: "sanam", isMale: true, address:" matatritha");
-    }
+            (byte, byte) result = FindMinMax(numbers);
 
-    void PrintSomthinf(string message)
-    {
-        Console.WriteLine(message);
-    }
-        // function overlapig two function with same name
-    float Multiply(float firstNum, float secondNum)
-    {
-        float product =firstNum * secondNum;
-
-        return product;
-    }
-
-    float Multiply(float firstNum, float secondNum, float thirdNum)
-    {
-        float product =firstNum * secondNum * thirdNum ;
-
-        return product;
-    }
-
-    // for variable numbers of arguments use params key word
-    double sum(params double[]  numbers)
-    {
-        double sum=0;
-        foreach(double num in numbers)
-        {
-             sum = sum + num;
+            PrintCustomerDetails(age: 23, name: "sanam", isMale: true, address: " matatritha");
         }
-        return sum;
-    }
 
-    // optional arguments
-
-    string Greet(string name, string prefix = "Miss")
+        void PrintSomthinf(string message)
         {
-        string fullName =$"{prefix}, {name}"; // String interpolation
+            Console.WriteLine(message);
+        }
+        // function overlapig two function with same name
+        float Multiply(float firstNum, float secondNum)
+        {
+            float product = firstNum * secondNum;
+
+            return product;
+        }
+
+        float Multiply(float firstNum, float secondNum, float thirdNum)
+        {
+            float product = firstNum * secondNum * thirdNum;
+
+            return product;
+        }
+
+        // for variable numbers of arguments use params key word
+        double sum(params double[] numbers)
+        {
+            double sum = 0;
+            foreach (double num in numbers)
+            {
+                sum = sum + num;
+            }
+            return sum;
+        }
+
+        // optional arguments
+
+        string Greet(string name, string prefix = "Miss")
+        {
+            string fullName = $"{prefix}, {name}"; // String interpolation
 
             return fullName;
         }
 
         //  return multiple values : usint tuples
 
-        (byte, byte)   FindMinMax (byte[] numbers)
+        (byte, byte) FindMinMax(byte[] numbers)
         {
-         byte max =   numbers.Max();
-         byte min = numbers.Min();
+            byte max = numbers.Max();
+            byte min = numbers.Min();
 
-         return (min , max);
-            
-        }   
+            return (min, max);
 
-        void PrintCustomerDetails( string name,string address, byte age, bool isMale)
+        }
+
+        void PrintCustomerDetails(string name, string address, byte age, bool isMale)
         {
             Console.WriteLine($"{name},{age},{address}");
         }
@@ -91,6 +95,9 @@ class Methods
 
 
 
+
+
+    }
 
 
 }
