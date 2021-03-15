f1fc49c470584b0d1f95d0dfef385fd1ef55403e
sanam.udash55 <shaw.udash@gmail.com>
2021-03-15 16:17:06 +0545

    statemanagement in mvc model in server side through cookie and query strings

--- EmployeeManagement/Controllers/StateManagementController.cs
+++ EmployeeManagement/Controllers/StateManagementController.cs
@@ -4,30 +4,46 @@ using Microsoft.AspNetCore.Mvc;
 
 public class StateManagementController: Controller
 {
-    public IActionResult firstpage(string name ,int age)
+        // parameter in method will appear as the query in url 
+    public IActionResult FirstPage()
     {
-
         // server side 
 
+        
 
         // client side 
-        //1.cookies 
-        var userName = "sanam udash";
-      
-
-        CookieOptions options = new CookieOptions();
-        options.Expires = DateTime.Now.AddHours(1);
-        Response.Cookies.Append("user",userName);
+            // 1) cookie 
+            // setting cookiee in c# mvc dotnet core 
+            var userName = "sanam udash";
+            // creating the options object for the cookiee
+            CookieOptions options = new CookieOptions();
+            // setting Expires properties of the options to one hours from now 
+            options.Expires = DateTime.Now.AddHours(1);
+           // giving the cookie to the cookie with name user 
+           // response will set cookie 
+        Response.Cookies.Append("user",userName,options);    
 
 
         return View();
-
-       
     }
-     public ViewResult secondPage()
-        {
-            var user = Request.Cookies["user"];
 
-            return View(user);
-        }
+    public IActionResult SecondPage(string name , int age )
+    {
+        // // getting the cookie set by first page 
+        // var user = Request.Cookies["user"];
+    // first parameter view name second parameter model 
+        
+        // 2. Query strings : string in url 
+        Response.Cookies.Append("user1",name);
+       var user =  Request.Cookies["user1"];
+        return View("SecondPage",user);
+    }
+    [HttpPost]
+    // getting id from another page in hidden form 
+    public IActionResult SecondPage(string id)
+    {
+        return View("HiddenPage",id);
+    } 
+    
+
 }
